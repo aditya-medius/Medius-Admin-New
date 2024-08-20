29efa1a1bfecabb28b6d7927fd08d7f354cb51cf
aditya-medius <arawat@medius.in>
2024-08-20 18:18:40 +0530

    Added environment dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,32 +23,11 @@ export class AppComponent implements OnInit {
     // this.login();
   }
 
-  // onEnvChange = (url: string) => {
-  //   localStorage.setItem('apiUrl', url);
-  //   window.location.reload();
-  // };
-
-  // public localUrl: string = apiUrl;
-  // login = () => {
-  //   let url: string = `${apiUrl}/admin/login?phoneNumber=8826332442&password=123456`;
-  //   return this.http.put(url, {}).subscribe((result: any) => {
-  //     if (result.status == 200) {
-  //       this.toastrService.success(`${result.message}`);
-  //       localStorage.setItem('admin', JSON.stringify(result.data));
-  //     } else if (result.status == 400) {
-  //       if (result.type == 'JsonWebTokenError') {
-  //         this.toastrService.error('Invalid OTP');
-  //       }
-  //       this.toastrService.error(result.message);
-  //     }
-  //   });
-  // };
-
   onEnvChange = (url: string) => {
     localStorage.setItem('apiUrl', url);
     localStorage.removeItem('admin');
     this.router.navigate(['/auth']);
-    // window.location.reload();
+    window.location.reload();
   };
   public localUrl: string = apiUrl;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   // apiUrl: 'http://3.134.79.163:3000',
   // apiUrl: 'http://18.218.162.226:3000',
-  apiUrl: ' http://15.207.210.44:3000',
+  // apiUrl: ' http://15.207.210.44:3000',
+  apiUrl: "http://13.235.135.237:3000"
 };
